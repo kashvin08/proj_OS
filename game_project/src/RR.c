e11243b3178351ec79b08c3abc0aac5950ec08b5
kashvin08 <kashvin116300@gmail.com>
2026-01-29 13:36:12 +0800

    Refactor RR.c for improved readability and structure

--- game_project/src/RR.c
+++ game_project/src/RR.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum){
-  int rem_bt[n];
-  for (int i = 0; i < n; i++){
-    rem_bt[i] = bt[i];
-  }
-  int t = 0;
+void findWaitingTime(int processes[], int n, int bt[], int wt[], int quantum)
+{
+    int rem_bt[n];
 
-  while(1) {
-    bool done = true;
-    for(int i = 0; i < n; i++){
-      if(rem_bt[i] > 0){
-        done = false;
-          if(rem_bt[i] > quantum){
-            t += quantum;
-            rem_bt[i] -= quantum;
-          }
-      else{
-        t = t + rem_bt[i];
-        wt[i] = t - bt[i];
-        rem_bt[i] = 0; 
-      }
-   }
-}
-if (done = true)
-  break;
-  }
+    for (int i = 0; i < n; i++)
+        rem_bt[i] = bt[i];
+
+    int t = 0;
+
+    while (1)
+    {
+        bool done = true;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (rem_bt[i] > 0)
+            {
+                done = false;
+
+                if (rem_bt[i] > quantum)
+                {
+                    t += quantum;
+                    rem_bt[i] -= quantum;
+                }
+                else
+                {
+                    t += rem_bt[i];
+                    wt[i] = t - bt[i];
+                    rem_bt[i] = 0;
+                }
+            }
+        }
+
+        if (done == true)
+            break;
+    }
 }
 
 void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
@@ -36,36 +47,36 @@ void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[])
 
 void findavgTime(int processes[], int n, int bt[], int quantum)
 {
-    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+    int wt[n], tat[n];
+    int total_wt = 0, total_tat = 0;
 
     findWaitingTime(processes, n, bt, wt, quantum);
-
     findTurnAroundTime(processes, n, bt, wt, tat);
 
-  cout << "PN\t "
-         << " \tBT "
-         << "  WT "
-         << " \tTAT\n";
+    printf("PN\tBT\tWT\tTAT\n");
 
     for (int i = 0; i < n; i++)
     {
-        total_wt = total_wt + wt[i];
-        total_tat = total_tat + tat[i];
-        cout << " " << i + 1 << "\t\t" << bt[i] << "\t " << wt[i] << "\t\t " << tat[i] << endl;
+        total_wt += wt[i];
+        total_tat += tat[i];
+        printf("%d\t%d\t%d\t%d\n",
+               processes[i], bt[i], wt[i], tat[i]);
     }
 
-    cout << "Average waiting time = " << (float)total_wt / (float)n;
-    cout << "\nAverage turn around time = " << (float)total_tat / (float)n;
+    printf("\nAverage waiting time = %.2f",
+           (float)total_wt / n);
+    printf("\nAverage turn around time = %.2f\n",
+           (float)total_tat / n);
 }
 
 int main()
 {
     int processes[] = {1, 2, 3};
-    int n = sizeof processes / sizeof processes[0];
+    int n = sizeof(processes) / sizeof(processes[0]);
 
     int burst_time[] = {10, 5, 8};
-
     int quantum = 2;
+
     findavgTime(processes, n, burst_time, quantum);
     return 0;
 }
